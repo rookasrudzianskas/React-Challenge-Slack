e08ffdcd96eec42d473488c08f409fdcb21d17a5
Rokas <rookas.rudzenskas@gmail.com>
2021-05-20 18:24:39 +0300

    Rokas

---
+++ src/data/SidebarData.js
@@ -0,0 +1,7 @@
+import MessageIcon from '@material-ui/icons/Message';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleIcon from '@material-ui/icons/People';
+import AppsIcon from '@material-ui/icons/Apps';
+
