2e16fa56bc73f4adcf6242056868b6e79fe9b0c4
Rokas <rookas.rudzenskas@gmail.com>
2021-05-31 08:20:48 +0300

    Rokas

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import "./styles/Chat.css";
+import styled from "styled-components";
 
 const Chat = () => {
     return (
-        <div>
-Chat
-        </div>
+        <Container>
+            <Header>
+
+            </Header>
+
+            <MessageContainer>
+
+            </MessageContainer>
+
+            <ChatInput>
+
+            </ChatInput>
+        </Container>
+
     );
 };
 
 export default Chat;
+
+
+const Container = styled.div`
+  
+`
+const Header = styled.div`
+  
+`
+
+const MessageContainer = styled.div`
+  
+`
+
+const ChatInput = styled.div`
+  
+`
+
+
